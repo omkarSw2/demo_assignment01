0ab213e7c51aa4b4faf01014d025c2f0a2ec3abc
omkarSw2 <omkar21143@gmail.com>
2022-12-07 19:10:56 +0530

    first push

--- pellindrom.js
+++ pellindrom.js
@@ -1,1 +1,11 @@
+let arr="afifa";
+bag="";
+for(i=arr.length-1;i>=0;i--){
 
+
+}
+if(arr==bag){
+  console.log("Pallindrome");
+}else{
+  console.log("Not Pallindrome");
+    }
--- prime.js
+++ prime.js
@@ -1,1 +1,13 @@
-
+let a=13;  
+ let count=0;
+    let count1=0;
+for(i=0;i<=a;i++){
+    if(a%i==0){
+        count;
+    }
+}
+if(count==2){
+    console.log("True");
+}else {
+    console.log("False");
+}
